e75c155c3845847d3c8ee406146f3a85f40aacff
yonatan kreiner <yonatanKreiner@gmail.com>
2017-06-27 20:06:57 +0300

    refactor

--- main.py
+++ main.py
@@ -1,24 +1,20 @@
 # pylint: skip-file
 # Requires PyAudio and PySpeech.
 
-import speech_recognition as sr
+import speech_recognition
 
-#he-il
-lang = 'es-es'
+lang = 'he-il'
+recognizer = speech_recognition.Recognizer()
 
-# Record Audio
-r = sr.Recognizer()
-with sr.Microphone() as source:
-    print("Say something!")
-    audio = r.listen(source)
+def record():
+    with speech_recognition.Microphone() as source:
+        print("Say something!")
+        return recognizer.listen(source)
 
-# Speech recognition using Google Speech Recognition
 try:
-    # for testing purposes, we're just using the default API key
-    # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
-    # instead of `r.recognize_google(audio)`
-    print("You said: " + r.recognize_google(audio, language=lang))
-except sr.UnknownValueError:
+    audio = record()
+    print("You said: " + recognizer.recognize_google(audio, language=lang)) # key="GOOGLE_SPEECH_RECOGNITION_API_KEY
+except speech_recognition.UnknownValueError:
     print("Google Speech Recognition could not understand audio")
-except sr.RequestError as e:
-    print("Could not request results from Google Speech Recognition service; {0}".format(e))
+except speech_recognition.RequestError as err:
+    print("Could not request results from Google Speech Recognition service; {0}".format(err))
